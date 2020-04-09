54923b00362eb535275c099a27b53a0923bf3898
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-09 23:09:11 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
     if array[count] == value_to_find
       found_value_index = count
     end
-    count ++
+    count += 1
   end
   return found_value_index
 end
